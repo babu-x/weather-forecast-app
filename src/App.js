8f4f725ce68b32a72496145c4f343a9fe4e45ce8
Bhaskar Pasupurathi <bhaskar170863@gmail.com>
2023-05-13 23:49:07 +0530

    weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Nabvar";
+import Sidebar from "./components/Sidebar";
+import Main from "./components/Main";
+import SearchLocation from "./components/SearchLocation";
+import "./App.css";
 
-function App() {
+import { useState } from "react";
+
+const App = () => {
+  const [isClickedHambugerIocn, setIsClickedHambugerIocn] = useState(false);
+  const [favList, setFavList] = useState([]);
+  const [currentLocation, setCurrentLocation] = useState("");
+
+  console.log(favList);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <BrowserRouter>
+        <Navbar
+          isClickedHambugerIocn={isClickedHambugerIocn}
+          setIsClickedHambugerIocn={setIsClickedHambugerIocn}
+          currentLocation={currentLocation}
+        />
+        <div className="weather-body-container">
+          {!isClickedHambugerIocn && <Sidebar />}
+          <div className="routes-container">
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={
+                  <Main
+                    favList={favList}
+                    setCurrentLocation={setCurrentLocation}
+                  />
+                }
+              />
+              <Route
+                exact
+                path="/search"
+                element={<SearchLocation setFavList={setFavList} />}
+              />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
